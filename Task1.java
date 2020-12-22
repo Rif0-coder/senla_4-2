09b250053c8dba7d8b0b217fd43a6043e74e71ae
Бедушевский Ян Дмитриевич <C:\Users\Bedushevski\AppData\Roaming\The Bat!>
2020-12-22 12:37:15 +0300

    fix

--- Task1.java
+++ Task1.java
@@ -5,21 +5,22 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        // Программа должна записать все введенные числа в массив. Вывести номера в диапазоне от 99 до 199
+        // Программа должна записать все введенные числа в массив.
+        // Вывести номера в диапазоне от 99 до 199
         Scanner in = new Scanner(System.in);
         System.out.println("kol-vo elementov");
         int a = in.nextInt();
-
         int[] massive = new int[a];
 
         for (int i = 0; i < massive.length; i++) {
-            massive[i] = in.nextInt();
+            // random value
+            massive[i] = ((int)(Math.random()*200));
         }
         System.out.println("mas" + Arrays.toString(massive));
 
         for (int counter = 0; counter < massive.length; counter++) {
             if (massive[counter] >= 99 && massive[counter] <= 199) {
-                System.out.print(massive[counter]);
+                System.out.print(massive[counter]+"\t");
             }
         }
     }
